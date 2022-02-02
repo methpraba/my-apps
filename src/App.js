82302bb378752df06f6bb21bda734f685331289b
Methmi Prabavi <94212042+methpraba@users.noreply.github.com>
2022-02-02 09:48:16 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { AccountBox } from "./components/accountBox";
+import image from './images/back.gif';
+
+
+
+const AppContainer = styled.div`
+  width: auto;
+  height: auto;
+  display: flexbox;
+  align-items: center;
+  overflow:auto;
+  justify-content: center;
+  background-size:cover;
+  min-height: 100vh;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ backgroundImage:`url(${image})`}}>
+    <AppContainer>
+      <AccountBox />
+    </AppContainer></div>
   );
 }
 
